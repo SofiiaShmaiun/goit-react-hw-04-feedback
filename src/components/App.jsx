c2484d5e64e2c9fb0e6a6b016c92740a1af97eba
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-05-18 21:59:06 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,54 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const leaveFeedback = label => {
+    if (label === 'good') {
+      setGood(good + 1);
+    } else if (label === 'neutral') {
+      setNeutral(neutral + 1);
+    } else if (label === 'bad') {
+      setBad(bad + 1);
+    }
   };
 
-  leaveFeedback = label => {
-    this.setState({ [label]: this.state[label] + 1 });
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback = () => {
-    return this.state.good + this.state.neutral + this.state.bad;
-  };
+  const totalFeedback = countTotalFeedback();
 
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-        }}
-      >
-        <FeedbackOptions
-          options={['good', 'bad', 'neutral']}
-          onLeaveFeedback={this.leaveFeedback}
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+      }}
+    >
+      <FeedbackOptions
+        options={['good', 'bad', 'neutral']}
+        onLeaveFeedback={leaveFeedback}
+      />
+
+      {good > 0 || bad > 0 || neutral > 0 ? (
+        <Statistics
+          onCountTotalFeedback={totalFeedback}
+          good={good}
+          neutral={neutral}
+          bad={bad}
         />
-
-        {this.state.good > 0 || this.state.bad > 0 || this.state.neutral > 0 ? (
-          <Statistics
-            onCountTotalFeedback={totalFeedback}
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
 }
